e86cd55c497cbd79bfbbf078d2c7eb8985e4950e
oussamato98 <oussamaaouad98@gmail.com>
2023-07-18 18:26:53 +0100

    REST +  set of authentication with facebook

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate')
-
+const FacebookStrategy = require('passport-facebook');
 
 
 
@@ -24,10 +24,10 @@ app.use(session({
     secret: 'Oursecret',
     resave: false,
     saveUninitialized: true
-  }))
+}))
 
- app.use(passport.initialize());
- app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
@@ -35,7 +35,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 const userSchema = new mongoose.Schema({
 
     username: String,
-    password: String
+    password: String,
+    googleId: String,
+    facebookId: String
 
 });
 
@@ -46,89 +48,130 @@ userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy());
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, {
-        id: user.id,
-        username: user.username,
-        picture: user.picture
-      });
-    });
-  });
-  
-  passport.deserializeUser(function(user, cb) {
-    process.nextTick(function() {
-      return cb(null, user);
-    });
-  });
 
+
+passport.use(User.createStrategy());
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, {
+            id: user.id,
+            username: user.username,
+            picture: user.picture
+        });
+    });
+});
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+
+
+// Configuration de la strategie d authentifictaion google 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        // When strategy want to find a user with googleID they cant so they created a new user because we dont have googleID in our scheme of user to let him find through
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            //Une fois que l'utilisateur est trouvé ou créé avec succès, la fonction de vérification appelle cb(null, user) pour indiquer à Passport que l'authentification est réussie.
+            return cb(err, user);
+        });
+    }
 ));
 
 
-app.get('/',function(req,res){
+// Configuration de la strategie d authentifictaion facebook 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+
+app.get('/', function (req, res) {
     res.render('home');
 })
 
+
+
+
+
+// Déclenche le processus d'authentification OAuth avec Google preconfigure dans la strategie , si l utilisateur s est bien authentifie et accepte d acceder a son scope: ['profile'] rediriger vers le callbackURL
 app.get("/auth/google",
-    passport.authenticate("google",{ scope: ['profile'] })
+    //la methode .authenticate dans ce cas va initialiser l authentification avec google 
+    passport.authenticate("google", { scope: ['profile'] })
 );
+app.get('/auth/facebook',
+    passport.authenticate('facebook', { scope: ['public_profile'] }));
 
-app.get('/auth/google/secrets', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect secrets page.
-    res.redirect('/secrets');
-  });
 
-app.get('/login',function(req,res){
+
+
+
+
+app.get('/auth/google/secrets',
+    //la metohode .authenticate dans ce cas va vérifier les informations d'authentification reçues de Google et récupérer les informations de profil de l'utilisateur.
+    passport.authenticate("google", { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect secrets page.
+        res.redirect('/secrets');
+    });
+
+app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/secrets');
+    });
+
+app.get('/login', function (req, res) {
     res.render('login');
 })
 
-app.get('/register',function(req,res){
+app.get('/register', function (req, res) {
     res.render('register');
 })
 
-app.get('/secrets',function(req,res){
-    if(req.isAuthenticated()){
+app.get('/secrets', function (req, res) {
+    if (req.isAuthenticated()) {
         res.render('secrets');
     }
-    else{
+    else {
         res.redirect("/login")
     }
 })
 
-app.get('/logout', function(req, res){
+app.get('/logout', function (req, res) {
 
-  req.logout(function(err) {
-    if (err) { 
-        console.log(err);
-     }
-     else {
+    req.logout(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
 
-        res.redirect('/');
+            res.redirect('/');
 
-     }
-  });
-    
-  });
+        }
+    });
 
-app.post('/register',function(req,res){
-    
-    User.register({username:req.body.username}, req.body.password , function(err, user){
+});
 
-        if(err){
+app.post('/register', function (req, res) {
+
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+
+        if (err) {
             console.log(err);
             res.redirect('/register');
         }
@@ -138,39 +181,35 @@ app.post('/register',function(req,res){
             // passport.authenticate("google") : pour l'authentification avec Google OAuth.
             // passport.authenticate("facebook") : pour l'authentification avec Facebook OAuth.
             // .authenticate cree une session d utilisateur et la stocke dans un cockie dans la reponse pour qu elle soit accessible par d autre methode
-            passport.authenticate("local")(req,res,function(){
+            passport.authenticate("local")(req, res, function () {
                 res.redirect('/secrets');
             });
         }
 
     })
 
+})
+app.post("/login", function (req, res) {
 
-     
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+    // le plugin passportLocalMongoose ajoute automatiquement des méthodes telles que User.authenticate() à votre modèle User pour la vérification des informations d'identification.
+
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect('/secrets');
+            });
+        }
+    });
 
 
 })
- app.post("/login",function(req,res){
-
-    const user = new User({
-        username:req.body.username,
-        password:req.body.password
-    })
-        // le plugin passportLocalMongoose ajoute automatiquement des méthodes telles que User.authenticate() à votre modèle User pour la vérification des informations d'identification.
-    
-        req.login(user, function(err) {
-        if (err) {
-             console.log(err);
-             }
-        else {
-            passport.authenticate("local")(req,res,function(){
-                res.redirect('/secrets');
-            });
-        }
-      });
-
-
- })
 
 
 
